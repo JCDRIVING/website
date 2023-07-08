74c6d03cf184f4b85e238fa0f7676362a86d467b
MovingBox <toladoltis@gmail.com>
2023-07-08 09:58:12 -0400

    modified

--- jcdriving_school/settings.py
+++ jcdriving_school/settings.py
@@ -118,16 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media/')
-
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
